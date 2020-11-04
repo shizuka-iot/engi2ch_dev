988ecb338aebc32c2fe59054cdbc66751897f14d
shizuka-iot <tatakitubusu@gmail.com>
2020-11-04 13:44:43 +0900

    利用規約ファイルとviewページを作成した。スタイルはほとんど設定してない。

---
+++ public/agreement.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__.'/../config/config.php';
+
+include 'views/layout/head.php';
+include 'views/layout/header.php';
+include 'views/agreement_view.php';
+include 'views/layout/footer.php';
+
+?>
+
