844e1a0ad26a9187fa34650d52ac2dfcbf08d188
shizuka-iot <tatakitubusu@gmail.com>
2020-11-04 13:45:51 +0900

    このサイトについてファイルおよびページを作成。スタイルの設定はまだ

---
+++ public/views/about_view.php
@@ -0,0 +1,15 @@
+<div class="center main">
+	<div class="container column center">
+		<div class="about_wrap">
+			<h1>
+				このサイトについて
+			</h1>
+
+			<ul>
+				<li>このサイトはプログラマー・エンジニアのための匿名掲示板です。シンプルさにこだわり登録型のSNSでは言いにくい内容も匿名なので利用規約の範囲内であれば話題にできます。</li>
+				<li>当サイトは２ちゃんねるおよび５ちゃんねるとは全く別のサイトです。</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
