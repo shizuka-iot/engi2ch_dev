1435f2f4156e0478f538ac48431d6610569a883b
shizuka-iot <tatakitubusu@gmail.com>
2020-11-07 02:30:55 +0900

    コンタクトフォームのデザインを更に整えた

--- public/views/contact_view.php
+++ public/views/contact_view.php
@@ -4,29 +4,38 @@
 <div class="center main">
 	<div class="container column center">
 		<div class="contact_form_wrap center column">
-			<h2>お問い合わせ</h2>
-			<form action="" method="post" class="contact_form center column">
+			<h2 class="">お問い合わせ<i class="fas fa-envelope"></i></h2>
+			<form action="" method="post" class="contact_form">
 				<input type="hidden" name="recaptcha_response" id="recaptchaResponse">
-				<br>
-				<p>メールアドレス <span class="require">必須</span></p>
-				<input type="text" name="email" placeholder="メールアドレス" required>
 
-				<br>
-				<br>
-				<p>名前 <span class="require">必須</span></p>
-				<input type="text" name="name" placeholder="名前" required>
+				<div class="input_wrap">
+					<p>メールアドレス <span class="require">必須</span></p>
+					<input type="text" name="email" placeholder="メールアドレス" required>
+				</div>
 
-				<br>
-				<br>
-				<p>件名 <span class="require">必須</span></p>
-				<input type="text" name="subject" placeholder="件名" required>
 
-				<br>
-				<br>
-				<p>本文 <span class="require">必須</span></p>
-				<textarea id="body" name="body" cols="30" rows="10" required></textarea>
-				<br>
-				<button type="submit" name="submit">お問い合わせ内容を送信する</button>
+				<div class="input_wrap">
+					<p>名前 <span class="require">必須</span></p>
+					<input type="text" name="name" placeholder="名前" required>
+				</div>
+
+
+				<div class="input_wrap">
+					<p>件名 <span class="require">必須</span></p>
+					<input type="text" name="subject" placeholder="件名" required>
+				</div>
+
+
+				<div class="input_wrap">
+					<p>本文 <span class="require">必須</span></p>
+					<textarea id="body" name="body" cols="30" rows="10" required></textarea>
+				</div>
+
+
+				<div class="">
+				<button type="submit" class="send_contact_button" name="submit">お問い合わせ内容を送信する</button>
+				</div>
+
 			</form>
 		</div>
 	</div>
