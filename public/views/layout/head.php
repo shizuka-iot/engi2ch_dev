e68d83e7dede02664651e21d0dc97d3b370cb68f
shizuka-iot <tatakitubusu@gmail.com>
2020-11-08 18:16:43 +0900

    googleアナリティクスのタグをheadタグの先頭に追加

--- public/views/layout/head.php
+++ public/views/layout/head.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-E5TYHJCCQR"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-E5TYHJCCQR');
+	</script>
+
+
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?=h(SITE_TITLE)?></title>
