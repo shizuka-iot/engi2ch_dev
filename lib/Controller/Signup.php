0b4d7108df34d0406f20ff91c2fa7656c0f85ac8
shizuka-iot <tatakitubusu@gmail.com>
2021-03-06 23:11:16 +0900

    PSR-2に準拠するように修正

--- lib/Controller/Signup.php
+++ lib/Controller/Signup.php
@@ -1,14 +1,16 @@
 <?php
 namespace Mvc0623\Controller;
 
+/*
+ * 新規登録メソッド
+ * 今回は使わない
+ */
 class Signup extends \Mvc0623\Controller
 {
 	public function run()
 	{
-		if( $_SERVER['REQUEST_METHOD'] === 'POST' )
-		{
-			if( isset($_POST['signup']) )
-			{
+		if ($_SERVER['REQUEST_METHOD'] === 'POST')	{
+			if( isset($_POST['signup']) )	{
 				$this->_signup();
 			}
 		}
@@ -16,18 +18,17 @@ class Signup extends \Mvc0623\Controller
 
 	private function _signup()
 	{
-		try
-		{
+		try	{
 			$val = $this->_validate();
-			if( $this->hasError())
-			{
+
+			if ($this->hasError()) {
 				return;
 			}
+
 			$Thread = new \Mvc0623\Model\User();
 			$Thread->create($val['username'], $val['email'], $val['password']);
 		}
-		catch(\Exception $e)
-		{
+		catch (\Exception $e) {
 			$this->setError('email', $e->getMessage());
 			return;
 		}
@@ -40,36 +41,28 @@ class Signup extends \Mvc0623\Controller
 		$this->validateToken();
 
 		$username = filter_input(INPUT_POST, 'username');
-		if( $username === '')
-		{
+		if ($username === '') {
 			$this->setError('username', '入力してください');
 		}
-		else
-		{
+		else {
 			$this->setValue('username', $username);
 		}
-		// var_dump($username);
-
 
 		$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-		if( $email )
-		{
+
+		if ($email) {
 			$this->setValue('email', $email);
 		}
-		else
-		{
+		else {
 			$this->setError('email', '正しいメールアドレスを入力してください');
 		}
-		// var_dump($email);
-
 
 		$password = filter_input(INPUT_POST, 'password');
-		if( !preg_match('/\A[a-zA-z0-9]{4,8}\z/', $_POST['password']) )
-		{
+
+		if (!preg_match('/\A[a-zA-z0-9]{4,8}\z/', $_POST['password'])) {
 			$this->setError('password', '半角英数4-8文字');
 		}
-		else
-		{
+		else {
 			$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 		}
 		return ['username'=>$username, 'email'=>$email, 'password'=>$password];
