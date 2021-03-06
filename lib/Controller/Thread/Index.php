040183637d96e146bb2f3ebf47f0d568d6225634
shizuka-iot <tatakitubusu@gmail.com>
2021-03-07 00:32:27 +0900

    PSR-2に準拠するように修正

--- lib/Controller/Thread/Index.php
+++ lib/Controller/Thread/Index.php
@@ -7,35 +7,29 @@ class Index extends \Mvc0623\Controller\Thread
 	{
 		$this->_redirect();
 
-		if( $_SERVER['REQUEST_METHOD'] === 'POST' )
-		{
-			if( isset($_POST['create_thread']) )
-			{
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			if( isset($_POST['create_thread']) ) {
 				$this->createThread();
 			}
-			if( isset($_POST['reply']) )
-			{
+			if( isset($_POST['reply']) ) {
 				$this->reply();
 			}
 		}
 	}
 
+	/*
+	 * セッション変数のsuccessキーがtrueだったらリダイレクト処理
+	 */
 	private function _redirect()
 	{
-		// セッション変数のsuccessが存在していれば括弧内の処理を行う。
-		// 多分postがあったらsuccessに値を入れるのだと思うが、
-		// どういう処理をしたのか覚えていない。
-		if( isset($_SESSION['success']) )
-		{
+		if ( isset($_SESSION['success']) ) {
 			// 中身が真なら中身を空にしてからリダイレクト
-			if( $_SESSION['success'] === true )
-			{
+			if( $_SESSION['success'] === true ) {
 				unset($_SESSION['success']);
 				header('Location:'.SITE_URL.'#');
 				exit;
 			}
 		}
 	}
-
 }
 ?>
