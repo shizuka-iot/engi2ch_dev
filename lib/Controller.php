ddd2183f175a79a7c4ff476b8452c840a7eb5713
shizuka-iot <tatakitubusu@gmail.com>
2021-03-06 22:28:51 +0900

    PSR-2に準拠するように修正。コメントも変更

--- lib/Controller.php
+++ lib/Controller.php
@@ -6,17 +6,22 @@ class Controller
 	private $_values;
 	private $_errors;
 
+	/*
+	 * コンストラクタ
+	 */
 	public function __construct()
 	{
 		$this->_createToken();
 
+		// stdClass()は連想配列のようにして使える
 		$this->_values = new \stdClass();
 		$this->_errors = new \stdClass();
 	}
 
-	/*************************************************************
-								トークン生成とバリデート
-	*************************************************************/
+
+	/*
+	 * トークン生成メソッド
+	 */
 	private function _createToken()
 	{
 		if( !isset($_SESSION['token']) )
@@ -24,6 +29,11 @@ class Controller
 			$_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(16));
 		}
 	}
+
+
+	/*
+	 * トークン検証メソッド
+	 */
 	protected function validateToken()
 	{
 		if( !isset($_SESSION['token']) || !isset($_POST['token']) ||
@@ -35,10 +45,9 @@ class Controller
 	}
 
 
-
-	/*************************************************************
-											セッターとゲッター
-	*************************************************************/
+	/*
+	 * セッター
+	 */
 	protected function setValue($key, $value)
 	{
 		$this->_values->$key = $value;
@@ -47,6 +56,9 @@ class Controller
 	{
 		$this->_errors->$key = $error;
 	}
+	/*
+	 * ゲッター
+	 */
 	public function getValue($key)
 	{
 		return isset($this->_values->$key) ? $this->_values->$key : '';
@@ -55,39 +67,66 @@ class Controller
 	{
 		return isset($this->_errors->$key) ? $this->_errors->$key : '';
 	}
+
+	/*
+	 * エラーの有無を判定するメソッド
+	 * 有無を判定するだけで実際にどういう処理をするかは別の部分で行う
+	 */
 	protected function hasError()
 	{
 		return !empty(get_object_vars($this->_errors));
 	}
-	/*************************************************************
-											エラーを表示
-	*************************************************************/
+
+
+	/*
+	 * エラー表示メソッド
+	 * this->_errorsオブジェクトのキーを指定すると
+	 * そのプロパティを取り出してhtml上に出力する
+	 *
+	 * @param string $key // this->_errorsのキー
+	 */
 	public function showError($key)
 	{
 		$html = '<p class="err">'.$this->getError($key).'</p>';
 		echo $html;
 	}
 
-	/*************************************************************
-											ログイン関連
-	*************************************************************/
+
+	/*
+	 * ログイン判定メソッド
+	 * 今回は使わない
+	 */
 	protected function isLoggedIn()
 	{
 		return isset($_SESSION['me']) && !empty($_SESSION['me']);
 	}
+
+
+	/*
+	 * セッションに入ってるログイン情報を返すメソッド
+	 * ログイン状態を判定し
+	 * コントローラーからセッションにログイン情報を入れるので
+	 * それを取り出すのだが今回は使わない
+	 */
 	public function me()
 	{
 		return $this->isLoggedIn() ? $_SESSION['me'] : null;
 	}
 
+
+	/*
+	 * スレッドに投稿された画像のurlを取り出すメソッド
+	 * サムネイルフラグを確認してサムネが原寸かを返す
+	 *
+	 * @param string $fileName
+	 * @param bool $thumbnail_flag
+	 */
 	public function getImgUrl($fileName, $thumbnail_flag)
 	{
-		if( $thumbnail_flag )
-		{
+		if ($thumbnail_flag) {
 			return basename(THUMBS_DIR).'/'.$fileName;
 		}
-		else
-		{
+		else {
 			return basename(IMGS_DIR).'/'.$fileName;
 		}
 	}
